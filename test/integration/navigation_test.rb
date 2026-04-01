aabbf905f54b710ee865dc142b74992aa1d83f1e
jeremymwood <jeremy.mason.wood@gmail.com>
2026-03-31 20:48:20 -0500

    feat: add cart quantity badge to nav link

--- test/integration/navigation_test.rb
+++ test/integration/navigation_test.rb
@@ -8,7 +8,7 @@ class NavigationTest < ActionDispatch::IntegrationTest
     assert_select "nav.site-nav"
     assert_select "a.site-nav-link", text: "Home"
     assert_select "a.site-nav-link", text: "Shop"
-    assert_select "a.site-nav-link", text: "Cart"
+    assert_select "a.site-nav-link", text: "Cart (0)"
     assert_select "a.site-nav-link.active", text: "Home"
   end
 
@@ -30,6 +30,14 @@ class NavigationTest < ActionDispatch::IntegrationTest
     get "/cart"
 
     assert_response :success
-    assert_select "a.site-nav-link.active", text: "Cart"
+    assert_select "a.site-nav-link.active", text: "Cart (0)"
+  end
+
+  test "updates cart nav count after adding item" do
+    post "/cart/items", params: { product_id: "abyss-selvedge-14oz", quantity: 2 }
+    follow_redirect!
+
+    assert_response :success
+    assert_select "a.site-nav-link", text: "Cart (2)"
   end
 end
